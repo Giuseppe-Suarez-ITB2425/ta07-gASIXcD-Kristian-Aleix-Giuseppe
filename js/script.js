a9a7fe68e41634e3d1b62daf45b0a0a5298b4bf1
Giuseppe-Suarez-ITB2425 <stalyn.suarez.7e8@itb.cat>
2025-02-27 21:28:25 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -446,11 +446,11 @@ function formatDate(date) {
 async function loadCategoryData(category) {
     try {
         const jsonFiles = {
-            water: './water-data.json',
-            cleaning: './cleaning-data.json',
-            office: './office-data.json',
-            services: './services-data.json',
-            internet: './internet-data.json'
+            water: 'data/water-data.json',
+            cleaning: 'data/cleaning-data.json',
+            office: 'data/office-data.json',
+            services: 'data/services-data.json',
+            internet: 'data/internet-data.json'
         };
         
         console.log('Loading:', jsonFiles[category]);
